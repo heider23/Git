f9a243af57be0ce6b23c05f0fd2939490db6c479
Heider Taguatinga Farias <heider23@hotmail.com>
2024-02-07 23:09:52 -0300

    First implementation.

---
+++ HelloWorld/src/Hello.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!!!");
+		System.exit(0);
+	}
+
+}
